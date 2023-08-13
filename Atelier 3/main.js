b872cbde54ba033385f57c6e4916e4b24660a713
Moustapha Ndiaye <mikeend08@gmail.com>
2023-08-13 17:31:39 +0000

    atelier 3

---
+++ Atelier 3/main.js
@@ -0,0 +1,23 @@
+const button1 = document.getElementsByClassName("index1");
+const button2 = document.getElementsByClassName("index2");
+const button3 = document.getElementsByClassName("index3");
+const button4 = document.getElementsByClassName("index4");
+const button5 = document.getElementsByClassName("index5");
+
+  button1.addEventListener('click', () => {
+    div.style.borderRadius = '5px';
+  });
+  button2.addEventListener('click', () => {
+    div.style.borderRadius = '10px';
+  });
+  button3.addEventListener('click', () => {
+    div.style.borderRadius = '15px';
+  });
+  button4.addEventListener('click', () => {
+    div.style.borderRadius = '20px';
+  });
+  button5.addEventListener('click', () => {
+    div.style.borderRadius = '25px';
+  });
+
+
