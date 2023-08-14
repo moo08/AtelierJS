9c023af592a18a18056372b9439ffcf6e59750bb
Moustapha Ndiaye <mikeend08@gmail.com>
2023-08-14 00:05:48 +0000

    tache refaite

--- Atelier 3/main.js
+++ Atelier 3/main.js
@@ -1,23 +1,69 @@
-const button1 = document.getElementsByClassName("index1");
-const button2 = document.getElementsByClassName("index2");
-const button3 = document.getElementsByClassName("index3");
-const button4 = document.getElementsByClassName("index4");
-const button5 = document.getElementsByClassName("index5");
 
-  button1.addEventListener('click', () => {
-    div.style.borderRadius = '5px';
+const youpi = document.querySelector(".box")
+
+const button1 = document.getElementById("index1");
+const button2 = document.querySelector("#index2");
+const button3 = document.querySelector("#index3");
+const button4 = document.querySelector("#index4");
+const button5 = document.querySelector("#index5");
+
+  button1.addEventListener('mouseover', () => {
+    youpi.classList.add('button1')
+    //button1.style.borderRadius = '5px';
+
   });
-  button2.addEventListener('click', () => {
-    div.style.borderRadius = '10px';
+  
+  button1.addEventListener('mouseout', () => {
+    youpi.classList.remove('button1')
+    //button1.style.borderRadius = '5px';
+
   });
-  button3.addEventListener('click', () => {
-    div.style.borderRadius = '15px';
+//button 2
+  button2.addEventListener('mouseover', () => {
+    youpi.classList.add('button2')
+    //button1.style.borderRadius = '5px';
+
   });
-  button4.addEventListener('click', () => {
-    div.style.borderRadius = '20px';
+  
+  button2.addEventListener('mouseout', () => {
+    youpi.classList.remove('button2')
+    //button1.style.borderRadius = '5px';
+
   });
-  button5.addEventListener('click', () => {
-    div.style.borderRadius = '25px';
+//button 3
+  button3.addEventListener('mouseover', () => {
+    youpi.classList.add('button3')
+    //button1.style.borderRadius = '5px';
+
+  });
+  
+  button3.addEventListener('mouseout', () => {
+    youpi.classList.remove('button3')
+    //button1.style.borderRadius = '5px';
+
+  });
+//button4
+  button4.addEventListener('mouseover', () => {
+    youpi.classList.add('button4')
+    //button1.style.borderRadius = '5px';
+
+  });
+  
+  button4.addEventListener('mouseout', () => {
+    youpi.classList.remove('button4')
+    //button1.style.borderRadius = '5px';
+
   });
 
+  //button 5
+  button5.addEventListener('mouseover', () => {
+    youpi.classList.add('button5')
+    //button1.style.borderRadius = '5px';
 
+  });
+  
+  button5.addEventListener('mouseout', () => {
+    youpi.classList.remove('button5')
+    //button1.style.borderRadius = '5px';
+
+  });
